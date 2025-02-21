08be367102f13ae37bb88b11c58adf6e3a51d319
Jay <kshevitskiy@gmail.com>
2025-02-21 16:29:06 +0100

    chore(dashboard): create app layouts

---
+++ dashboard/src/layouts/base.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Wrapper from "@/components/Wrapper.vue";
+</script>
+
+<template>
+  <Wrapper>
+    <slot />
+  </Wrapper>
+</template>
---
+++ dashboard/src/layouts/search.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Base from "@/layouts/base.vue";
+import Navigation from "@/components/Navigation.vue";
+import Sidebar from "@/components/Sidebar.vue";
+import Navbar from "@/components/Navbar.vue";
+import Bar from "@/components/Bar.vue";
+</script>
+
+<template>
+  <Base>
+    <div class="grid items-start gap-12 xl:flex">
+      <Sidebar class="xl:min-w-xs">
+        <Bar>
+          <Navbar />
+        </Bar>
+        <Navigation />
+      </Sidebar>
+
+      <main class="min-h-screen xl:w-9/12">
+        <slot name="bar" />
+
+        <slot />
+      </main>
+    </div>
+  </Base>
+</template>
