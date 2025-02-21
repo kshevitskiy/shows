39874aad0c130b26e109c38be9690ad9d524d78f
Jay <kshevitskiy@gmail.com>
2025-02-21 16:29:26 +0100

    chore(dashboard): extend app router

--- dashboard/src/router/index.ts
+++ dashboard/src/router/index.ts
@@ -1,23 +1,48 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import SearchIndexView from "@/views/SearchIndexView.vue";
+
+export enum RouteNames {
+  HOME = "home",
+  SEARCH_INDEX = "search_index",
+  SEARCH_GENRE_ID = "search_genre_id",
+  SHOW_ID = "show_id",
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: RouteNames.HOME,
       component: HomeView,
+      children: [
+        {
+          path: "",
+          name: RouteNames.SEARCH_INDEX,
+          component: SearchIndexView,
+        },
+        {
+          path: "/search/genre/:id",
+          name: RouteNames.SEARCH_GENRE_ID,
+          component: () => import("../views/SearchGenreIdView.vue"),
+        },
+      ],
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: "/show/:id",
+      name: RouteNames.SHOW_ID,
+      component: () => import("../views/ShowView.vue"),
     },
   ],
-})
+  scrollBehavior(to, from, savedPosition) {
+    // if navigating back, restore previous scroll position
+    if (savedPosition) {
+      return savedPosition;
+    }
+    // otherwise, scroll to top
+    return { top: 0, behavior: "smooth" }; // Smooth scrolling
+  },
+});
 
-export default router
+export default router;
