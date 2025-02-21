1b353d0f273d8d6270647ca3aaad7bec8b1812d1
Jay <kshevitskiy@gmail.com>
2025-02-21 21:41:55 +0100

    fix(dashboard): handle empty array in getLargestBackgroundImage method

--- dashboard/src/utils/showImages.ts
+++ dashboard/src/utils/showImages.ts
@@ -4,12 +4,14 @@ export function getRandomBackgroundImage(images: ImageDTO[]) {
 }
 
 export function getLargestBackgroundImage(images: ImageDTO[]) {
-  return images
-    .filter((i) => i.type === "background" && i.resolutions.original)
-    .reduce((largest, current) =>
-      current.resolutions.original.width * current.resolutions.original.height >
-      largest.resolutions.original.width * largest.resolutions.original.height
-        ? current
-        : largest
-    );
+  const filteredImages = images.filter((i) => i.type === "background" && i.resolutions?.original);
+
+  if (filteredImages.length === 0) return undefined;
+
+  return filteredImages.reduce((largest, current) =>
+    current.resolutions?.original.width * current.resolutions?.original.height >
+    largest.resolutions?.original.width * largest.resolutions?.original.height
+      ? current
+      : largest
+  );
 }
