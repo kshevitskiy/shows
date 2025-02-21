242f53c482dd1503f4cadaff14b1f212993c0740
Jay <kshevitskiy@gmail.com>
2025-02-22 00:25:25 +0100

    chore(dashboard): tweak cypress config and add cucumber packages

--- dashboard/cypress.config.ts
+++ dashboard/cypress.config.ts
@@ -1,8 +1,29 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+
+export async function setupNodeEvents(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+) {
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createEsbuildPlugin(config)],
+    })
+  );
+
+  return config;
+}
 
 export default defineConfig({
   e2e: {
-    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173',
+    setupNodeEvents,
+    baseUrl: "http://localhost:5173",
+    specPattern: "cypress/e2e/features/**/*.feature",
+    video: false,
   },
-})
+});
